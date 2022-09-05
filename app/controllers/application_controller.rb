703c5d017017ecccf429db9e17dc703af4d898e2
Elizabeth Wandia Mugo <wwandia173@gmail.com>
2022-09-05 13:25:52 +0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
+  post '/reviews' do
+    binding.pry
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json  
+  end
+  
   get '/games' do
     games = Game.all.order(:title).limit(10)
     games.to_json
@@ -14,6 +26,21 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  patch '/reviews/:id' do
+    review =  Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
   end
 
 end
